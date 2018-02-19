93b3e4e06dee879d97eafb94016dfc254690ee2f
vishnupriya97 <34455259+vishnupriya97@users.noreply.github.com>
2018-02-19 20:28:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('article1', function (req, res) {
+app.get('/article1', function (req, res) {
   res.send(path.join('ariticle1 requested and will be served'));
 });
 
